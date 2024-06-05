e3e2f6661626b7b66c3afb2faa6148154efbe69d
Carl Westman <carl.westman91@gmail.com>
2024-06-05 10:48:19 +0000

    Changed name of manual test file so that that pytest does not pick it up

--- tests/manual_test_ng_air.py
+++ tests/manual_test_ng_air.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ctek.nanogrid_air import NanogridAir
+from ctek import NanogridAir
 
 
 @pytest.mark.asyncio
@@ -24,6 +24,8 @@ async def test_nanogrid_air_print_mac():
 @pytest.mark.asyncio
 async def test_nanogrid_air_print_meter_data():
     meter_data = await NanogridAir().fetch_meter_data()
+    print(meter_data.active_power_in)
+
     print(f"Meter data: {meter_data}")
 
 
